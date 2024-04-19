3632f58134968af262aa00e0982114b175a4a0b0
gitelam <smiotics@gmail.com>
2024-04-18 17:01:44 -0700

    added config.js

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const EMAIL = "smiotics@gmail.com";
+export const LINKEDIN = "https://www.linkedin.com/in/samuel-hiram-castro-708982253/";
+export const GITHUB = "https://github.com/gitelam";
